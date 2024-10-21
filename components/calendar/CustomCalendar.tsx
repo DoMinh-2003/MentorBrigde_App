5b85beed3a7420ed56c8bb232e82884ccd685141
baotran1311 <baotran13112003@gmail.com>
2024-10-21 23:50:22 +0700

    Feat/Tran: List Metting

---
+++ components/calendar/CustomCalendar.tsx
@@ -0,0 +1,90 @@
+import { Calendar, Agenda, LocaleConfig } from "react-native-calendars";
+import { FC, useState } from "react";
+import { View } from "react-native";
+interface CalendarProps {
+  selected?: string;
+  setSelected?: (date: string) => void;
+}
+const CustomCalendar: FC<CalendarProps> = ({ selected, setSelected }) => {
+  return (
+    <View>
+      <Calendar
+        style={{
+          borderColor: "gray",
+          height: 330,
+          borderRadius: 20,
+        }}
+        enableSwipeMonths={true}
+        theme={{
+          textSectionTitleColor: "#000000",
+          textSectionTitleFontWeight: "900",
+          selectedDayBackgroundColor: "#00adf5",
+          selectedDayTextColor: "#ffffff",
+          todayTextColor: "#FF6001",
+          dayTextColor: "#000000",
+          textDisabledColor: "#AAAAAA",
+          arrowColor: "#FF6001",
+        }}
+        onDayPress={(day) => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {
+            selected: true,
+            disableTouchEvent: true,
+            // selectedDotColor: "#FF6001",
+            selectedColor: "#FF6001",
+          },
+        }}
+      />
+    </View>
+  );
+};
+
+export default CustomCalendar;
+
+// Configure locale for Vietnamese
+LocaleConfig.locales["vi"] = {
+  monthNames: [
+    "Tháng 1",
+    "Tháng 2",
+    "Tháng 3",
+    "Tháng 4",
+    "Tháng 5",
+    "Tháng 6",
+    "Tháng 7",
+    "Tháng 8",
+    "Tháng 9",
+    "Tháng 10",
+    "Tháng 11",
+    "Tháng 12",
+  ],
+  monthNamesShort: [
+    "Th.1",
+    "Th.2",
+    "Th.3",
+    "Th.4",
+    "Th.5",
+    "Th.6",
+    "Th.7",
+    "Th.8",
+    "Th.9",
+    "Th.10",
+    "Th.11",
+    "Th.12",
+  ],
+  dayNames: [
+    "Chủ Nhật",
+    "Thứ Hai",
+    "Thứ Ba",
+    "Thứ Tư",
+    "Thứ Năm",
+    "Thứ Sáu",
+    "Thứ Bảy",
+  ],
+  dayNamesShort: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
+  today: "Hôm nay",
+};
+
+// Set Vietnamese as the default locale
+LocaleConfig.defaultLocale = "vi";
--- screens/mentorScreens/schedule/MentorSchedule.tsx
+++ screens/mentorScreens/schedule/MentorSchedule.tsx
@@ -1,95 +1,105 @@
+import CustomCalendar from "@/components/calendar/CustomCalendar";
+import { LinearGradient } from "@/components/ui/gradient/LinearGradient";
+import { useState } from "react";
 import { Text, View } from "react-native";
 import { ScrollView } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { Calendar, Agenda, LocaleConfig } from "react-native-calendars";
-import { useState } from "react";
-
-// Configure locale for Vietnamese
-LocaleConfig.locales["vi"] = {
-  monthNames: [
-    "Tháng 1",
-    "Tháng 2",
-    "Tháng 3",
-    "Tháng 4",
-    "Tháng 5",
-    "Tháng 6",
-    "Tháng 7",
-    "Tháng 8",
-    "Tháng 9",
-    "Tháng 10",
-    "Tháng 11",
-    "Tháng 12",
-  ],
-  monthNamesShort: [
-    "Th.1",
-    "Th.2",
-    "Th.3",
-    "Th.4",
-    "Th.5",
-    "Th.6",
-    "Th.7",
-    "Th.8",
-    "Th.9",
-    "Th.10",
-    "Th.11",
-    "Th.12",
-  ],
-  dayNames: [
-    "Chủ Nhật",
-    "Thứ Hai",
-    "Thứ Ba",
-    "Thứ Tư",
-    "Thứ Năm",
-    "Thứ Sáu",
-    "Thứ Bảy",
-  ],
-  dayNamesShort: ["CN", "T2", "T3", "T4", "T5", "T6", "T7"],
-  today: "Hôm nay",
-};
-
-// Set Vietnamese as the default locale
-LocaleConfig.defaultLocale = "vi";
-
+import { Box } from "@/components/ui/box";
+import { Card } from "@/components/ui/card";
+import { Button, ButtonText } from "@/components/ui/button";
 function MentorSchedule() {
   const insets = useSafeAreaInsets();
   const [selected, setSelected] = useState("");
   return (
-    <ScrollView style={{ paddingTop: insets.top }} className="m-[15px]">
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      nestedScrollEnabled={true}
+      style={{ paddingTop: insets.top }}
+      className="m-[15px]"
+    >
       <Text className="font-extra-bold-cereal text-2xl font-bold mb-5">
         Lịch trình
       </Text>
-      <View>
-        <Calendar
-          style={{
-            borderColor: "gray",
-            height: 350,
-            borderRadius: 20,
-          }}
-          enableSwipeMonths={true}
-          theme={{
-            textSectionTitleColor: "#b6c1cd",
-            selectedDayBackgroundColor: "#00adf5",
-            selectedDayTextColor: "#ffffff",
-            todayTextColor: "#FF6001",
-            dayTextColor: "#2d4150",
-            textDisabledColor: "#AAAAAA",
-            arrowColor: "#FF6001",
-          }}
-          onDayPress={(day) => {
-            setSelected(day.dateString);
-          }}
-          markedDates={{
-            [selected]: {
-              selected: true,
-              disableTouchEvent: true,
-              selectedDotColor: "orange",
-            },
-          }}
-        />
+
+      <CustomCalendar selected={selected} setSelected={setSelected} />
+      <View className="mt-4 w-full">
+        <Text className="font-extra-bold-cereal text-2xl font-bold mb-4">
+          Thông tin cuộc họp
+        </Text>
+
+        <LinearGradient
+          className="rounded-full items-center w-32 h-10 flex justify-center p-2 mt-1"
+          colors={["#FF6001", "#FF9759"]}
+          start={[0.5, 0]}
+          end={[0.5, 1]}
+        >
+          <Text className="text-white font-semibold">{selected}</Text>
+        </LinearGradient>
       </View>
-      <View>
-        <Text>Thông tin chi tiết</Text>
-        <Text>{selected}</Text>
+
+      <View className="mt-5 w-full ">
+        <Text className="text-xl font-extra-bold-cereal mt-5 font-medium">
+          Danh sách cuộc họp trong ngày
+        </Text>
+
+        <View className="h-[380px]">
+          <ScrollView
+            nestedScrollEnabled={true}
+            scrollEnabled={true}
+            showsVerticalScrollIndicator={true}
+            persistentScrollbar={true}
+            className="mt-4 "
+          >
+            <Box className="h-12 w-full rounded-full  border border-[#D5D5D7] flex-row items-center justify-between px-2 mb-5">
+              <Text className="font-bold">07:30-09:00</Text>
+              <Button
+                className="rounded-full"
+                variant="solid"
+                action="primary"
+                style={{ backgroundColor: "#EBE173" }}
+              >
+                <ButtonText>Đánh giá giảng viên</ButtonText>
+              </Button>
+            </Box>
+            <Box className="h-12 w-full rounded-full  border border-[#D5D5D7] flex-row items-center justify-between px-2 mb-5">
+              <Text className="font-bold">13:30-15:30</Text>
+              <Button
+                className="rounded-full"
+                variant="solid"
+                action="primary"
+                style={{ backgroundColor: "#EBE173" }}
+              >
+                <ButtonText>Đánh giá giảng viên</ButtonText>
+              </Button>
+            </Box>
+
+            <LinearGradient
+              className="rounded-full flex-row items-center w-full h-12 flex  items-center justify-between p-2 mt-1"
+              colors={["#71E0FF", "#2B8FD8"]}
+              start={[0, 0]}
+              end={[1, 1]}
+            >
+              <Text className="text-white font-bold">15:30-17:00</Text>
+              <View className="flex-row">
+                <Button
+                  className="rounded-full"
+                  variant="solid"
+                  action="primary"
+                >
+                  <ButtonText>Dời lịch</ButtonText>
+                </Button>
+                <Button
+                  className="rounded-full"
+                  variant="solid"
+                  action="primary"
+                  style={{ backgroundColor: "white" }}
+                >
+                  <ButtonText style={{color:"black"}}>Đến phòng họp</ButtonText>
+                </Button>
+              </View>
+            </LinearGradient>
+          </ScrollView>
+        </View>
       </View>
     </ScrollView>
   );
